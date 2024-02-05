fa0fe801419974762bd4fdf33fdc2ff291e9c553
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-05 16:35:04 +0300

    all char types example

---
+++ columntypes/initial-data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE all_char_types_table (
+    varcharColumn VARCHAR(255),
+    charColumn CHAR(10),
+    nvarcharColumn NVARCHAR(100),
+    ncharColumn NCHAR(5),
+    textColumn TEXT,
+    ntextColumn NTEXT
+);
---
+++ docker-compose/init-sql/sqlserver/ankara-db-init.sql
@@ -0,0 +1,52 @@
+﻿USE [master]
+GO
+
+IF DB_ID('ankara') IS NOT NULL
+  set noexec on
+
+CREATE DATABASE [ankara];
+GO
+
+USE [ankara]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE LOGIN [migrator] WITH PASSWORD = 'migrator123!'
+GO
+
+CREATE SCHEMA app
+GO
+
+CREATE USER [migrator] FOR LOGIN [migrator] WITH DEFAULT_SCHEMA=[app]
+GO
+
+EXEC sp_addrolemember N'db_owner', N'migrator'
+GO
+
+--CREATE LOGIN [ankaraUser] WITH PASSWORD = 'user123!'
+--GO
+--
+--CREATE USER [ankaraUser] FOR LOGIN [ankaraUser] WITH DEFAULT_SCHEMA=[app]
+--GO
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'myworker')
+BEGIN
+    CREATE TABLE myworker (
+        id INT NOT NULL,
+        name VARCHAR(45),
+        ssn VARCHAR(45),
+        PRIMARY KEY (id)
+    );
+END;
+GO
+
+INSERT INTO myworker (id,name,ssn) VALUES (1, 'orcun', '208');
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'myworker_backup')
+BEGIN
+    CREATE TABLE myworker_backup (id INT NOT NULL, name VARCHAR(45), PRIMARY KEY (id));
+END;
